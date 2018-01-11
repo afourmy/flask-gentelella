9e226fb5d0e0d36b1054722e7562c55b774976d2
afourmy <antoine.fourmy@gmail.com>
2018-01-11 06:58:24 +0100

    blueprint home

---
+++ source/home/routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template
+
+blueprint = Blueprint(
+    'home_blueprint', 
+    __name__, 
+    url_prefix = '/home', 
+    template_folder = 'templates',
+    static_folder = 'static'
+    )
+
+@blueprint.route('/<template>')
+def route_template(template):
+    return render_template(template)
