b8400cf528b35435a5bf2ee7e10afbbc09745e88
afourmy <antoine.fourmy@gmail.com>
2018-03-05 17:40:22 +0100

    pep8 improvements part 1

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,6 @@
-bind = "0.0.0.0:5100"
+bind = '0.0.0.0:5100'
 workers = 1
-accesslog = "-"
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
-loglevel = "debug"
+accesslog = '-'
+loglevel = 'debug'
 capture_output = True
 enable_stdio_inheritance = True
--- source/app.py
+++ source/app.py
@@ -1,56 +1,61 @@
 from config import DebugConfig
-from flask import Flask, render_template
+from flask import Flask
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
 from importlib import import_module
 from logging import basicConfig, DEBUG, getLogger, StreamHandler
 from os.path import abspath, dirname, join, pardir
-from sys import dont_write_bytecode, path
+import sys
 
 # prevent python from writing *.pyc files / __pycache__ folders
-dont_write_bytecode = True
+sys.dont_write_bytecode = True
 
 path_source = dirname(abspath(__file__))
 path_parent = abspath(join(path_source, pardir))
-if path_source not in path:
-    path.append(path_source)
+if path_source not in sys.path:
+    sys.path.append(path_source)
 
 from database import db, create_database
 from base.routes import login_manager
-from base.models import User
+
 
 def register_extensions(app):
     db.init_app(app)
     login_manager.init_app(app)
+
 
 def register_blueprints(app):
     for module_name in ('forms', 'ui', 'home', 'tables', 'data', 'additional', 'base'):
         module = import_module('{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
+
 def configure_login_manager(app, User):
     @login_manager.user_loader
     def user_loader(id):
         return db.session.query(User).filter_by(id=id).first()
-    
+
     @login_manager.request_loader
     def request_loader(request):
         username = request.form.get('username')
         user = db.session.query(User).filter_by(username=username).first()
         return user if user else None
 
+
 def configure_database(app):
     create_database()
+    migrate = Migrate(app, db)
+
     @app.teardown_request
     def shutdown_session(exception=None):
         db.session.remove()
-    migrate = Migrate(app, db)
+
 
 def configure_logs(app):
     if not app.debug:
-        basicConfig(filename='error.log',level=DEBUG)
+        basicConfig(filename='error.log', level=DEBUG)
     logger = getLogger()
     logger.addHandler(StreamHandler())
+
 
 def create_app():
     app = Flask(__name__, static_folder='base/static')
@@ -63,11 +68,9 @@ def create_app():
     configure_logs(app)
     return app
 
+
 app = create_app()
 
+
 if __name__ == '__main__':
-    app.run(
-        host = '0.0.0.0',
-        port = 5000,
-        threaded = True
-        )
+    app.run(host='0.0.0.0', port=5000, threaded=True)
