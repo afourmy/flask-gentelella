a470b0e5ff372154e3a1061ca5f6000cb7a2c6e6
afourmy <antoine.fourmy@gmail.com>
2018-03-06 07:45:06 +0100

    test example update

---
+++ tests/test_example.py
@@ -0,0 +1,13 @@
+from test_base import check_pages, check_blueprints
+
+@check_pages('/', '/home/index')
+def test_pages(base_client):
+    # do something
+    base_client.post('/', data={})
+    # the pages are tested (GET request: 200) afterwards
+
+@check_blueprints('/forms', '/ui')
+def test_blueprints(base_client):
+    # do something
+    base_client.post('/', data={})
+    # the blueprints are tested (GET request: 200) afterwards
