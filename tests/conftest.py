c84fedb782306c835ae079f159ac93343c7d1693
minto <minto@hotmail.fr>
2018-03-18 09:50:38 +0100

    uncomment standard test

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,24 +15,24 @@ if path_app not in sys.path:
 from app import create_app
 
 
-# @fixture
-# def base_client():
-#     app = create_app()
-#     yield app.test_client()
-#     remove(join(path_test, 'database.db'))
+@fixture
+def base_client():
+    app = create_app()
+    yield app.test_client()
+    remove(join(path_test, 'database.db'))
 
 
-# @fixture
-# def user_client():
-#     app = create_app()
-#     client = app.test_client()
-#     create = {'username': '', 'password': '', 'create_account': ''}
-#     login = {'username': '', 'password': '', 'login': ''}
-#     with app.app_context():
-#         client.post('/login', data=create)
-#         client.post('/login', data=login)
-#         yield client
-#     remove(join(path_test, 'database.db'))
+@fixture
+def user_client():
+    app = create_app()
+    client = app.test_client()
+    create = {'username': '', 'password': '', 'create_account': ''}
+    login = {'username': '', 'password': '', 'login': ''}
+    with app.app_context():
+        client.post('/login', data=create)
+        client.post('/login', data=login)
+        yield client
+    remove(join(path_test, 'database.db'))
 
 
 @fixture
