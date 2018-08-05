dd0e3f7b3605d68a88816b6a628a224b909fc295
minto <minto@hotmail.fr>
2018-08-05 13:03:20 +0200

    fix selenium

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -3,7 +3,7 @@ from tests.test_base import urls
 
 # this test uses selenium to check, for all pages of gentelella, that there
 # are no severe entry in chromium logs.
-def tst_selenium(selenium_client):
+def test_selenium(selenium_client):
     for blueprint, pages in urls.items():
         for page in pages:
             selenium_client.get('http://127.0.0.1:5000' + blueprint + page)
