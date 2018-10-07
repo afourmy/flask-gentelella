b3d3178bad8c96b4da785d9616a8c0a0835c70cf
minto <minto@hotmail.fr>
2018-10-07 16:10:54 +0200

    update postgresql database in config

--- app/config.py
+++ app/config.py
@@ -7,6 +7,18 @@ class Config(object):
 class ProductionConfig(Config):
     DEBUG = False
 
+    # Database
+    SQLALCHEMY_DATABASE_URI = environ.get(
+        'GENTELELLA_DATABASE_URL',
+        'postgresql://{}:{}@{}:{}/{}'.format(
+            environ.get('GENTELELLA_DATABASE_USER', 'gentelella'),
+            environ.get('GENTELELLA_DATABASE_PASSWORD', 'gentelella'),
+            environ.get('GENTELELLA_DATABASE_HOST', 'localhost'),
+            environ.get('GENTELELLA_DATABASE_PORT', 5432),
+            environ.get('GENTELELLA_DATABASE_NAME', 'gentelella')
+        )
+    )
+
 
 class DebugConfig(Config):
     DEBUG = True
