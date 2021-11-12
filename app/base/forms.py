798de67917b965796040a11a4265450d31d114f5
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-11-12 13:58:04 +0100

    Update forms.py

--- app/base/forms.py
+++ app/base/forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, PasswordField
+from wtforms import StringField, PasswordField
 
 ## login and registration
 
 
 class LoginForm(FlaskForm):
-    username = TextField('Username', id='username_login')
+    username = StringField('Username', id='username_login')
     password = PasswordField('Password', id='pwd_login')
 
 
 class CreateAccountForm(FlaskForm):
-    username = TextField('Username', id='username_create')
-    email = TextField('Email')
+    username = StringField('Username', id='username_create')
+    email = StringField('Email')
     password = PasswordField('Password', id='pwd_create')
