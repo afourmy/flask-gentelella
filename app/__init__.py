31e820d53eba840faf323457da0545ffca97cf19
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-11-12 13:57:38 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -15,9 +15,10 @@ def register_extensions(app):
 
 
 def register_blueprints(app):
-    for module_name in ('base', 'forms', 'ui', 'home', 'tables', 'data', 'additional', 'base'):
+    for module_name in ('base', 'forms', 'ui', 'home', 'tables', 'data', 'additional'):
         module = import_module('app.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
+
 
 
 def configure_database(app):
